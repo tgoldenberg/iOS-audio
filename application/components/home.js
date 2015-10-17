a95f047f11b50c3c0e7cccee0d48bafe0f2826f4
tgoldenberg <thomasagoldenberg@gmail.com>
2015-10-17 15:11:23 -0400

    get initial project with ability to replay sounds

--- application/components/home.js
+++ application/components/home.js
@@ -1,7 +1,8 @@
-const React         = require('react-native');
-
+let React = require('react-native');
+let {NativeModules, NativeAppEventEmitter} = require('react-native');
+let AudioPlayerManager = NativeModules.AudioPlayerManager;
+let AudioRecorderManager = NativeModules.AudioRecorderManager;
 let {
-  AsyncStorage,
   Text,
   TextInput,
   TouchableHighlight,
@@ -10,11 +11,33 @@ let {
 } = React;
 
 class Home extends React.Component{
+  constructor(props) {
+    super(props);
+    this.props =  {
+      currentTime: 0.0,
+      recording: false,
+      stoppedRecording: false,
+      stoppedPlaying: false,
+      playing: false,
+      finished: false
+    };
+  }
+
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'blue',}}>
         <View style={{height: 300, backgroundColor: 'red', marginTop: 170}}>
-          <Text>HELLO WORLD</Text>
+          <TouchableHighlight
+            underlayColor='#eee'
+            onPress={()=> {
+              // TODO
+              console.log("RECORDER", NativeModules);
+              AudioPlayerManager.play('./en.lproj/english.mp3');
+            }}
+            >
+            <Text>HELLO WORLD</Text>
+
+          </TouchableHighlight>
         </View>
       </View>
     );
